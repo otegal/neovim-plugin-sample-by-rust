57e65e0ced20e7e25e6ef9b727838bb7d35f763f
otegal <gyouzakuuka@gmail.com>
2021-12-18 21:49:25 +0900

    Add `recv` method to EventHandler to accept Neovim input and execute Rust processing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use neovim_lib::{Neovim, NeovimApi, Session};
+
 #[derive(Debug, PartialEq)]
 struct Calculator;
 
@@ -15,8 +17,74 @@ impl Calculator {
     }
 }
 
+struct EventHandler {
+    nvim: Neovim,
+    calculator: Calculator,
+}
+
+impl EventHandler {
+    fn new() -> EventHandler {
+        let session = Session::new_parent().unwrap(); // In case of Err, want to panic
+        let nvim = Neovim::new(session);
+        let calculator = Calculator::new();
+
+        EventHandler { nvim, calculator }
+    }
+
+    fn recv(&mut self) {
+        let receiver = self.nvim.session.start_event_loop_channel();
+
+        for (event, values) in receiver {
+            match Messages::from(event) {
+                Messages::Add => {
+                    let nums = values
+                        .iter()
+                        .map(|v| v.as_i64().unwrap())
+                        .collect::<Vec<i64>>();
+                    let sum = self.calculator.add(nums);
+                    self.nvim
+                        .command(&format!("echo \"Sum: {}\"", sum.to_string()))
+                        .unwrap();
+                }
+                Messages::Sub => {
+                    let nums = values
+                        .iter()
+                        .map(|v| v.as_i64().unwrap())
+                        .collect::<Vec<i64>>();
+                    let sub_sum = self.calculator.sub(nums);
+                    self.nvim
+                        .command(&format!("echo \"Sum (Sub): {}\"", sub_sum.to_string()))
+                        .unwrap();
+                }
+                Messages::Unknown(event) => {
+                    self.nvim
+                        .command(&format!("echo \"Unknown command: {}\"", event))
+                        .unwrap();
+                }
+            }
+        }
+    }
+}
+
+enum Messages {
+    Add,
+    Sub,
+    Unknown(String),
+}
+
+impl From<String> for Messages {
+    fn from(event: String) -> Self {
+        match &event[..] {
+            "add" => Messages::Add,
+            "sub" => Messages::Sub,
+            _ => Messages::Unknown(event),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut event_handler = EventHandler::new();
+    event_handler.recv();
 }
 
 #[cfg(test)]
