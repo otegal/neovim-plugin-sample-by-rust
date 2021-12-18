8dd60184cad05a3e3f6ce3b7eb8189dc592eb635
otegal <gyouzakuuka@gmail.com>
2021-12-18 20:26:58 +0900

    add calculator and `add` method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+#[derive(Debug, PartialEq)]
+struct Calculator;
+
+impl Calculator {
+    fn new() -> Self {
+        Calculator {}
+    }
+
+    fn add(&self, nums: Vec<i64>) -> i64 {
+        nums.iter().sum()
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_calculator_new() {
+        assert_eq!(Calculator {}, Calculator::new())
+    }
+
+    #[test]
+    fn test_calculator_add() {
+        let cal = Calculator::new();
+        assert_eq!(10, cal.add(vec!(1, 2, 3, 4)));
+        assert_eq!(-10, cal.add(vec!(-1, -2, -3, -4)));
+    }
 }
